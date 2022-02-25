fed0713e510bb372e83edf216a7df17150f1acf6
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-02-25 15:25:13 -0500

    input form

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+// Selectors
+const addBtn = document.getElementById("add");
+const grid = document.querySelector(".grid");
+const page = document.querySelector(".app");
+const inputDiv = document.querySelector(".input-div");
+
 let myLibrary = [];
 
+// Functions
 function Book(name, author, pages, read) {
   this.name = name;
   this.author = author;
@@ -8,3 +15,13 @@ function Book(name, author, pages, read) {
 }
 
 function addBookToLibrary() {}
+
+// EventListeners
+addBtn.addEventListener("click", () => {
+  inputDiv.classList.add("darken");
+});
+inputDiv.addEventListener("click", (e) => {
+  if (e.target.classList.contains("darken")) {
+    inputDiv.classList.remove("darken");
+  }
+});
